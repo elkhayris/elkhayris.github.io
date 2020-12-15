aecb4e2fc11e19098b330bab8afc6cbbf16f27df
Sami Elkhayri <selkhayri@gmail.com>
2020-12-15 11:45:07 -0500

    php email - first check-in

---
+++ contact/contact.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_POST['submit'])){
+    $name = trim($_POST["name"]);
+    $email = trim($_POST["email"]);
+    $subject = trim($_POST["subject"]);
+    $message = trim($_POST["message"]);
+    
+}
+
+?>
---
+++ contact/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div>
+            <h1>Get in touch</h1>
+            <p>Please fill in the fields</p>
+            <form action="contact.php" method="post">
+                <input type="text" name="name" placeholder="Full Name"/>
+                <input type="email" name="email" placeholder="E-mail"/>
+                <input type="text" name="subject" placeholder="Subject"/>
+                <textarea name="message" placeholder="Enter Message"></textarea>
+                <button type="submit" name="submit">SEND E-MAIL</button>
+            </form>
+        </div>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
